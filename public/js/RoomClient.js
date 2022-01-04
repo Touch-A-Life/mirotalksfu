3e07f7e70f6a071abb86d23de142040262c1312c
Ch Ravi <ravi.ch@touchalife.org>
2022-01-04 18:28:19 +0530

    Audio changes

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -1074,7 +1074,7 @@ class RoomClient {
             s.onclick = function () {
                 var audienceRef = document.getElementById(peer_info.peer_id + '__enableAudience');
                 var speakerRef = document.getElementById(peer_info.peer_id + '__enableSpeaker');
-                thisRef.updatePeerInfo(peer_name, peer_info.peer_id, 'audio', false);
+                // thisRef.updatePeerInfo(peer_name, peer_info.peer_id, 'audio', false);
                 thisRef.peerAction('me', `${peer_info.peer_id}___pAudio`, 'mute');
                 speakerRef.style.display = 'none';
                 audienceRef.style.cssText = 'display: flex;left: 25px;cursor: pointer;color:red;';
