75a6555992ea442ad55a9f67c6670e1ee838fbaa
Ch Ravi <ravi.ch@touchalife.org>
2022-01-05 20:59:21 +0530

    Audio changes

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -1254,6 +1254,11 @@ class RoomClient {
         this.peer_info.peer_audio = status;
         let b = this.getPeerAudioBtn(peer_id);
         if (b) b.className = this.peer_info.peer_audio ? html.audioOn : html.audioOff;
+
+        if (!status) {
+            let peerSpeakingIcon = document.getElementById(peer_id + '__speaking');
+            if (peerSpeakingIcon) peerSpeakingIcon.style.display = 'none';
+        }
     }
 
     setIsVideo(status) {
