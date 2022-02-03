268a7473aac13dfaf96b312f97e6404b51d41540
Ch Ravi <ravi.ch@touchalife.org>
2022-02-03 22:23:34 +0530

    Wait for Moderator changes

--- public/js/Room.js
+++ public/js/Room.js
@@ -1497,7 +1497,7 @@ async function getParticipantsTable(peers) {
         let peer_eject = _PEER.ejectPeer;
         let peer_sendFile = _PEER.sendFile;
         let peer_id = peer_info.peer_id;
-        let currentIsModerator = rolesList.push(peer_info.isModerator);
+        let currentIsModerator = rolesList.push(peer_info.peer_isModerator);
         console.log('role value', peer_info, currentIsModerator);
 
         if (rc.peer_id === peer_id) {
